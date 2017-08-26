9523998f1f66880365e7ff417be5e5702fee3008
cspatil8 <patil.chinmay8@gmail.com>
2017-08-25 22:22:31 -0400

    Added core data entity Added NSManagedObject class

---
+++ todoList/TodoList.swift
@@ -0,0 +1,13 @@
+//
+//  TodoList.swift
+//  todoList
+//
+//  Created by Chinmay Patil on 8/25/17.
+//  Copyright © 2017 Chinmay Patil. All rights reserved.
+//
+
+import UIKit
+
+class TodoList: NSManagedObject {
+
+}
---
+++ todoList/todoListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  todoListTableViewCell.swift
+//  todoList
+//
+//  Created by Chinmay Patil on 8/20/17.
+//  Copyright © 2017 Chinmay Patil. All rights reserved.
+//
+
+import UIKit
+
+class todoListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
